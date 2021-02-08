a6ec1f3dc1144c9fdf628aec00dd3cc77b9fc416
Sebastian <sebastiano139@gmail.com>
2021-02-08 15:30:55 +0100

    Added session storage

--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -16,15 +16,17 @@ type AppContextInterface = [AppStateInterface, React.Dispatch<actionType>];
 const reducer = (state: AppStateInterface, action: actionType) => {
     switch (action.type) {
         case "SET_NAME":
+            sessionStorage.setItem("APP_CONTEXT", JSON.stringify({ ...state, name: action.name }));
             return { ...state, name: action.name };
         case "SET_COLOR":
+            sessionStorage.setItem("APP_CONTEXT", JSON.stringify({ ...state, color: action.color }));
             return { ...state, color: action.color };
         default:
             return state;
     }
 };
 
-const appInitialContext = {
+const appInitialContext = JSON.parse(sessionStorage.getItem("APP_CONTEXT") as string) || {
     name: "",
     color: ""
 };
